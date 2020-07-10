aa3454a9bbc21fca6062846075234fd21fb43fa0
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-07-10 09:42:49 -0700

    test written + code created for week

---
+++ __tests__/weekday-calculator.test.js
@@ -0,0 +1,11 @@
+import { weekdayCalc } from './../src/weekday-calculator.js';
+
+describe('weekdayCalc', function() {
+
+  it('it should return name of day of week based on weekdayValue ', function() {
+    let date = new Date(2019, 4, 12);
+    console.log(date);
+    console.log(date.getDay());
+    expect(weekdayCalc(date)).toEqual("Sunday");
+  });
+});
