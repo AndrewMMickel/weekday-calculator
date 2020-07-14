25dc37479cc07061b2b43cdfc61edbb6c9936767
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-07-14 09:59:48 -0700

    fixed testing, added business + UI logic

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
+import { weekdayCalc } from './weekday-calc';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $('#date-form').submit(function(event) {
+    event.preventDefault();
+    let month = $('#month').val();
+    let day = $('#day').val();
+    let year = $('#year').val();
+    let myDate = new Date(year, month, day);
+    let weekday = weekdayCalc(myDate);
+      $('#solution').text(`The day of the week is ${weekday}`);
+  });
+});
